d950080af1c7d0fc27230f4d85c8741ffddffaed
RafaGazziero <rafael.gazziero.ifpr.2023@gmail.com>
2025-08-27 15:14:58 -0300

    Criado múltipla escolha dias da semana

---
+++ Multipla_Escolha/js/ex00.js
@@ -0,0 +1,37 @@
+var dia
+
+dia = parseFloat(prompt("Digite o número do dia da semana correspondente (1 a 7): "))
+
+switch (dia) {
+    case 1:{
+        alert("Domingo")
+        break
+    }
+    case 2:{    
+        alert("Segunda-feira")
+        break
+    }
+    case 3:{    
+        alert("Terça-feira")
+        break
+    }
+    case 4:{    
+        alert("Quarta-feira")
+        break
+    }
+    case 5:{    
+        alert("Quinta-feira")
+        break
+    }
+    case 6:{    
+        alert("Sexta-feira")
+        break
+    }   
+    case 7:{    
+        alert("Sábado")
+        break
+    }
+    default:{
+        alert("Número inválido! Digite um número entre 1 e 7.")
+    }
+}
