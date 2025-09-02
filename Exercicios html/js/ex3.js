a779256f37a35a569eb5aa5cfdfad216cdaf8d21
RafaGazziero <rafael.gazziero.ifpr.2023@gmail.com>
2025-09-02 20:57:14 -0300

    Exercício 3 feito

---
+++ Exercicios html/js/ex3.js
@@ -0,0 +1,16 @@
+var altura, esporte
+
+altura = parseFloat(prompt("Digite a sua altura em metros: "))
+
+if(altura >= 1.80){
+    alert("Seu esporte é o basquete.")
+}
+
+
+if(altura <= 1.80){
+    alert("Seu esporte é o futebol.")
+}
+
+else{
+    alert("Você está apto para os dois esportes")
+}
