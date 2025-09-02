86314794dc0df6c2acce7e1ecd7bc5ffd5424e4b
RafaGazziero <rafael.gazziero.ifpr.2023@gmail.com>
2025-09-02 20:51:16 -0300

    Exercícios 1 e 2 feitos

---
+++ Exercicios html/js/ex1.js
@@ -0,0 +1,11 @@
+var numero, triplo
+
+numero = parseInt(prompt("Digite um numero: "))
+
+if(numero <= 3){
+ triplo =numero * 3
+ alert("O triplo do número é: "+ triplo)
+
+}else{
+    alert("Digite um número menor")
+}
---
+++ Exercicios html/js/ex2.js
@@ -0,0 +1,12 @@
+var numero, resultado
+
+numero = parseInt(prompt("Digite um numero: "))
+
+if(numero <= 3){
+ resultado = numero * 2
+ alert("O dobro é: "+ resultado)
+
+}else{
+    resultado = numero * 3
+    alert("O triplo é: "+ resultado)
+}
