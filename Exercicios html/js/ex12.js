bbceb8e655f09d441e5bbfe01339c97c74b0d9ff
RafaGazziero <rafael.gazziero.ifpr.2023@gmail.com>
2025-09-02 21:52:46 -0300

    correções no ex 12

--- Exercicios html/js/ex12.js
+++ Exercicios html/js/ex12.js
@@ -2,24 +2,24 @@ var n1, n2, op, resultado
 
 n1 = parseInt(prompt("Digite o primeiro valor: "))
 n2 = parseInt(prompt("Digite o segundo valor: "))
-op = parseInt(prompt("Digite o número o qual corresponde a operação que você deseja realizar:  1 - soma 2 - subtração 3 - multiplicação 4 - divisão"))
+op = parseInt(prompt("Digite o número o qual corresponde a operação que você deseja realizar:    1 - soma 2 - subtração 3 - multiplicação 4 - divisão"))
 
-if(op = 1){
+if(op == 1){
     resultado = n1 + n2
     alert("O resultado da soma é " + resultado)
 }
 
-if(op = 2){
+if(op == 2){
     resultado = n1 - n2
     alert("O resultado da subtração do segundo valor no primeiro é " + resultado)
 }
 
-if(op = 3){
+if(op == 3){
     resultado = n1 * n2
     alert("O resultado da multiplicação é " + resultado)
 }
 
-if(op = 4){
+if(op == 4){
     resultado = n1 / n2
     alert("O resultado da divisão do primeiro valor com o segundo é " + resultado)
 }
